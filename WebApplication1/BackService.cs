10a39a30a7d39cfa62e22a9048599d29ea6f2196
student <kuty-y4vy@yandex.ru>
2022-02-18 21:42:31 +0300

    add blazorServerApp

--- WebApplication1/BackService.cs
+++ WebApplication1/BackService.cs
@@ -20,7 +20,6 @@ public class BackService
             _time = time;
             _logger = logger;
             _locator = locator;
-            
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -35,9 +34,6 @@ public class BackService
                var sw = Stopwatch.StartNew();
                while (await timer.WaitForNextTickAsync(stoppingToken))
                {
-                   //string result = String.Empty;
-                   
-                   
                    AsyncRetryPolicy? policy = Policy
                        .Handle<Exception>()
                        .RetryAsync(2, onRetry: (exception, retryAttempt) =>
@@ -49,38 +45,10 @@ public class BackService
                    PolicyResult? result = await policy.ExecuteAndCaptureAsync(
                            token => Send(token, srv, sw), stoppingToken);
                    
-
                    if (result.Outcome == OutcomeType.Failure)
                    {
                        _logger.LogError(result.FinalException, "There was an error while sending email");
                    }
-
-                   
-                   /*try
-                   {
-                       try
-                       {
-                           await srv.Send("Kuty-y4vy@yandex.ru", "test service",
-                               $"{_time.LocalTime}({_time.TimeZone.DisplayName}) - Сервер работает уже {sw.Elapsed}, ({GC.GetTotalAllocatedBytes() / 1024 / 1024} MB)",
-                               stoppingToken);
-                           _logger.LogWarning("Сервер уже работает {0} {1}", sw.Elapsed, result);
-                       }
-                       catch (Exception e)
-                       {
-                           await srv.Send("Kuty-y4vy@yandex.ru", "test service", 
-                               $"{_time.LocalTime}({_time.TimeZone.DisplayName}) - Сервер работает уже {sw.Elapsed}, ({GC.GetTotalAllocatedBytes() / 1024 / 1024} MB)", 
-                               stoppingToken);
-                           result = "Повторная попытка";
-                           _logger.LogError(e, "Сервер работает уже {0} {1}",sw.Elapsed, result);
-                       }
-
-                   }
-                   catch (Exception e)
-                   {
-                       result = "Сообщение не доставлено";
-                       _logger.LogError(e, "Сервер работает уже {0} {1}",sw.Elapsed, result);
-                   }*/
-
                }
            }
         }
