56030f7ee610026f50aace90c0e8aa108b965647
dante72 <turbosuns7@gmail.com>
2022-02-28 21:57:13 +0300

    fix timer

--- HttpModels/Clock.cs
+++ HttpModels/Clock.cs
@@ -7,4 +7,6 @@ public class Clock : IClock
     public DateTime LocalTime => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZone);
 
     public TimeZoneInfo TimeZone { get; set; } = TimeZoneInfo.Utc;
+    
+    public DateTime DiscountDate { get; set; } = DateTime.Now + TimeSpan.FromMinutes(5);
 }
--- HttpModels/IClock.cs
+++ HttpModels/IClock.cs
@@ -5,4 +5,6 @@ public interface IClock
     public DateTime LocalTime { get; }
 
     public TimeZoneInfo TimeZone { get; set; }
+    
+    public DateTime DiscountDate { get; set; }
 }
